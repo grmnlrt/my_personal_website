68868e929e07de0f198d4263ec1b8de0d4acdc22
germainloret <germain.loret@gmail.com>
2017-12-04 10:03:01 +0100

    add font awesome gem

--- config.rb
+++ config.rb
@@ -22,6 +22,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+
 configure :build do
   activate :minify_css
   # Use relative URLs
